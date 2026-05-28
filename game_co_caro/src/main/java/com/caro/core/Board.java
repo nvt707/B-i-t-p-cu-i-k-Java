b8f2e1baf84d7c161816dfc21a41bb49702a11f8
nvt707 <trongngovan288@gmail.com>
2026-05-28 10:34:47 +0700

    Update Board.java

--- game_co_caro/src/main/java/com/caro/core/Board.java
+++ game_co_caro/src/main/java/com/caro/core/Board.java
@@ -4,7 +4,7 @@ public class Board {
     private int size;
     private char[][] grid;
 
-    public Board(int size) { // Truyền 3 hoặc 5
+    public Board(int size) { 
         this.size = size;
         this.grid = new char[size][size];
         for (int i = 0; i < size; i++)
@@ -20,17 +20,14 @@ public class Board {
         return false;
     }
 
-    // Giản lược hàm checkWin để code ngắn gọn
     public boolean checkWin(char symbol) {
-        // Logic kiểm tra hàng ngang, dọc, chéo tùy theo size (3 hoặc 5)
         return false;
     }
 
-    // Lấy giá trị của một ô
     public char getCell(int x, int y) {
         return grid[x][y];
     }
-    // Hoàn tác nước đi (dành cho Bot thử nghiệm trong đầu)
+    
     public void undoMove(int x, int y) {
         grid[x][y] = '-';
     }
